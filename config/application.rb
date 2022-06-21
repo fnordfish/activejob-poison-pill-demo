ad4c019e2f3b8a0a927478d23242e871838bc1fd
Robert Schulze <robert@dotless.de>
2022-06-21 10:58:49 +0200

    Use rails 7.0 defaults for easier switching between versions

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Activejobtest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
     config.active_job.queue_adapter = :delayed_job
 
